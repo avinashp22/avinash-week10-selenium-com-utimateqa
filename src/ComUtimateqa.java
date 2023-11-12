bfe9cc6c6d60145bbc69dc3ee1caa72f4a8a455c
avinashp22 <avinashp22@gmail.com>
2023-11-12 12:43:33 +0000

    avinash-week10-selenium-com-utimateqa

--- src/ComUtimateqa.java
+++ src/ComUtimateqa.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -7,7 +9,7 @@ import java.time.Duration;
 
 public class ComUtimateqa {
 
-    static String browser = "firefox"; // choose browser
+    static String browser = "chrome"; // choose browser
     static WebDriver driver; // declare globally
 
     public static void main(String[] args) {
@@ -26,7 +28,28 @@ public class ComUtimateqa {
         driver.manage().window().maximize(); // maximise the window
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));  //give implicit wait to driver
 
+        System.out.println(driver.getTitle()); // print tile
+        System.out.println(driver.getCurrentUrl()); // print current url
+        System.out.println(driver.getPageSource()); // print page source
 
+        WebElement signinLink = driver.findElement(By.linkText("Sign In")); // hover to login button
+        signinLink.click(); // click on link
+        System.out.println(driver.getCurrentUrl()); // print current url
+
+        WebElement email = driver.findElement(By.id("Email"));    //find email field and enter email by ID
+        email.sendKeys("avi@patel.com");   // type into box, action
+
+        WebElement password = driver.findElement(By.id("Password"));       //find password field and enter password by ID
+        password.sendKeys("Halllo123");    // type into box, action
+
+        WebElement loginLink = driver.findElement(By.linkText("LOG IN")); // hover to login button
+        loginLink.click(); // click on link
+
+        driver.navigate().to(baseURL); // tell driver to navigate to base url
+
+        driver.navigate().forward();// go forward to homepage
+        driver.navigate().back(); // go back a page to base url page
+        driver.navigate().refresh(); // refresh url
 
         driver.quit();  // close browser
     }
